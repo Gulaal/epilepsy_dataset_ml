e842e581b99789f3a3a5a87c7839b6f7b7bdaf0d
Gulaal <egorveschev315@gmail.com>
2026-04-06 14:03:59 +0300

    random forest model

---
+++ random_forest_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+
+beed_file_path = "BEED_Data.csv"
+beed_data = pd.read_csv(beed_file_path)
+
+X = beed_data.iloc[:, :16]
+y = beed_data.iloc[:, 16]
+
+print(np.bincount(y))
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0, train_size=0.8, stratify=y)
+
+model = RandomForestClassifier(n_estimators=100, random_state=1)
+model.fit(train_X, train_y)
+
+predictions = model.predict(val_X)
+
+accuracy = accuracy_score(val_y, predictions)
+print(f"Accuracy: {accuracy:.3f}")
+print(classification_report(val_y, predictions))
+print(conf_matr := confusion_matrix(val_y, predictions))
